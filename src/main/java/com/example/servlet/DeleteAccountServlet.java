c0a3e928000758596731e80bca91930ed6dc8012
surapaneniabhinav <surapaneni.abhinav@gmail.com>
2017-08-16 23:05:31 -0400

    Updated Bank App

--- src/main/java/com/example/servlet/DeleteAccountServlet.java
+++ src/main/java/com/example/servlet/DeleteAccountServlet.java
@@ -1,6 +1,7 @@
 package com.example.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,22 +11,22 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class FirstServlet extends HttpServlet {
+@WebServlet(urlPatterns={"/deleteAccountServlet"})
+
+public class DeleteAccountServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
-        String balance = req.getParameter("balance");
-        try {
+               try {
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
-            PreparedStatement statement = connection.prepareStatement("INSERT into account values(?,?,?)");
+            PreparedStatement statement = connection.prepareStatement("DELETE FROM account WHERE firstName = ? AND lastName = ? ");
             statement.setString(1, firstName);
             statement.setString(2, lastName);
-            statement.setDouble(3, Double.parseDouble(balance));
             statement.executeUpdate();
-            resp.getWriter().write("Account Created");
+            resp.getWriter().write("Account Deleted");
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
--- src/main/java/com/example/servlet/SecondServlet.java
+++ src/main/java/com/example/servlet/SecondServlet.java
@@ -5,10 +5,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SecondServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello From Second Servlet");
+        String firstName = req.getParameter("firstName");
+        String balance = req.getParameter("balance");
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
+            PreparedStatement statement = connection.prepareStatement("update account set balance =? where firstName =? ");
+            statement.setString(2, firstName);
+            statement.setDouble(1, Double.parseDouble(balance));
+            statement.executeUpdate();
+            resp.getWriter().write("Account Updated");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
